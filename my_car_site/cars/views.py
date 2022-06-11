1f638fb0259b73b1d57011c508511dccc44c1498
Murat Şahin <sahinmurat3@gmail.com>
2022-06-11 23:29:41 +0300

    add and delete commands in views

--- my_car_site/cars/views.py
+++ my_car_site/cars/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
 from . import models
 
 # Create your views here.
@@ -8,8 +9,26 @@ def list(request):
     return render(request, 'cars/list.html', context=context)
 
 def add(request):
-    return render(request, 'cars/add.html')
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+        models.Car.objects.create(brand=brand, year=year)
+        # if user submitted new car ---> List.html
+        return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/add.html')
 
 def delete(request):
-    return render(request, 'cars/delete.html')
+    if request.POST:
+        #delete the car
+        pk = request.POST['pk']
+        try:
+            models.Car.objects.get(pk=pk).delete()
+            return redirect(reverse('cars:list'))
+        except:
+            print('pk not found!')
+            return redirect(reverse('cars:list'))
+
+    else:
+        return render(request, 'cars/delete.html')
     
